8351e236ce278c3d61de57c41e3abb51759a8734
rodfer0x80 <rodrigolf080@gmail.com>
2025-04-20 09:36:13 +0100

    Added tests to BEncode.encodeBEncode; test passing 92/94

--- test/BEncodeSpec.hs
+++ test/BEncodeSpec.hs
@@ -12,7 +12,7 @@ import Control.Exception (evaluate)
 import Data.Aeson
 import Data.Map (fromList)
 
-import BEncode (BEncode (..), decodeBEncode)
+import BEncode (BEncode (..), decodeBEncode, encodeBEncode)
 
 
 spec :: Spec
@@ -112,11 +112,10 @@ spec = do
         decodeBEncode "d0:i0ee" `shouldBe` BDict [("", BInt 0)]
       it "should decode a dictionary with an empty string value" $
         decodeBEncode "d3:foo0:ee" `shouldBe` BDict [("foo", BString "")]
-      -- TODO: fix this
-      --it "should decode a dictionary with an empty list value" $
-        --decodeBEncode "d3:foollee" `shouldBe` BDict [("foo", BList [])]
+      it "should decode a dictionary with an empty list value" $
+        decodeBEncode "d3:foolee" `shouldBe` BDict [("foo", BList [])]
       it "should decode a dictionary with an empty dictionary value" $
-        decodeBEncode "d3:foodeee" `shouldBe` BDict [("foo", BDict [])]
+        decodeBEncode "d3:foodee" `shouldBe` BDict [("foo", BDict [])]
       it "should handle multiple key-value pairs" $
         decodeBEncode "d1:ai1e1:bi2ee" `shouldBe` BDict [("a", BInt 1), ("b", BInt 2)]
 
@@ -153,3 +152,73 @@ spec = do
         evaluate (decodeBEncode "d3:fo") `shouldThrow` anyException
       it "should error on truncated dictionary value" $
         evaluate (decodeBEncode "d3:fooi1") `shouldThrow` anyException
+
+
+  describe "encodeBEncode" $ do
+    describe "Integers" $ do
+      it "should encode a positive integer" $
+        encodeBEncode (BInt 69) `shouldBe` "i69e"
+      it "should encode zero" $
+        encodeBEncode (BInt 0) `shouldBe` "i0e"
+      it "should encode a negative integer" $
+        encodeBEncode (BInt (-69)) `shouldBe` "i-69e"
+      it "should encode a large positive integer" $
+        encodeBEncode (BInt 9223372036854775807) `shouldBe` "i9223372036854775807e"
+      it "should encode a large negative integer" $
+        encodeBEncode (BInt (-9223372036854775808)) `shouldBe` "i-9223372036854775808e"
+
+    describe "Strings" $ do
+      it "should encode a simple string" $
+        encodeBEncode (BString "hello") `shouldBe` "5:hello"
+      it "should encode an empty string" $
+        encodeBEncode (BString "") `shouldBe` "0:"
+      it "should encode a string with special characters" $
+        encodeBEncode (BString "hello:!@#$") `shouldBe` "10:hello:!@#$"
+      it "should encode a long string" $
+        encodeBEncode (BString (B.pack $ replicate 1000 'a')) `shouldBe` B.pack (show (1000 :: Int) ++ ":" ++ replicate 1000 'a')
+      -- TODO: fix this 
+      --it "should encode a string with multibyte UTF-8 characters" $
+         --encodeBEncode (BString "你好世界") `shouldBe` "12:你好世界" -- Assuming UTF-8 encoding where each Chinese character is 3 bytes
+
+    describe "Lists" $ do
+      it "should encode an empty list" $
+        encodeBEncode (BList []) `shouldBe` "le"
+      it "should encode a simple list of strings" $
+        encodeBEncode (BList [BString "one", BString "two", BString "three"]) `shouldBe` "l3:one3:two5:threee"
+      it "should encode a list of integers" $
+        encodeBEncode (BList [BInt 1, BInt 2, BInt 3]) `shouldBe` "li1ei2ei3ee"
+      it "should encode a mixed list" $
+        encodeBEncode (BList [BString "hello", BInt 666]) `shouldBe` "l5:helloi666ee"
+      it "should encode a nested list" $
+        encodeBEncode (BList [BList [BString "blz", BInt 420]]) `shouldBe` "ll3:blzi420eee"
+      it "should encode a list with an empty string" $
+        encodeBEncode (BList [BString ""]) `shouldBe` "l0:e"
+      it "should encode a list with an empty list" $
+        encodeBEncode (BList [BList []]) `shouldBe` "llee"
+      it "should encode a list with an empty dictionary" $
+        encodeBEncode (BList [BDict []]) `shouldBe` "ldee"
+
+
+    describe "Dictionaries" $ do
+      it "should encode an empty dictionary" $
+        encodeBEncode (BDict []) `shouldBe` "de"
+      it "should encode a simple dictionary (string keys), sorted" $
+        encodeBEncode (BDict [("foo", BString "bar"), ("hello", BInt 42)]) `shouldBe` "d3:foo3:bar5:helloi42ee"
+      it "should encode a dictionary with integer values, sorted" $
+        encodeBEncode (BDict [("one", BInt 1), ("two", BInt 2)]) `shouldBe` "d3:onei1e3:twoi2ee"
+      it "should encode a dictionary with mixed values, sorted" $
+        encodeBEncode (BDict [("one", BInt 1), ("two", BString "five")]) `shouldBe` "d3:onei1e3:two4:fivee"
+      it "should encode a nested dictionary, sorted" $
+        encodeBEncode (BDict [("jump", BDict [("up", BInt 13), ("down", BInt 666)])]) `shouldBe` "d4:jumpd4:downi666e2:upi13eee"
+      it "should encode a dictionary with keys requiring sorting" $
+        encodeBEncode (BDict [("b", BInt 2), ("a", BInt 1)]) `shouldBe` "d1:ai1e1:bi2ee"
+      it "should encode a dictionary with an empty string key" $
+        encodeBEncode (BDict [("", BInt 0)]) `shouldBe` "d0:i0ee"
+      it "should encode a dictionary with an empty string value" $
+        encodeBEncode (BDict [("foo", BString "")]) `shouldBe` "d3:foo0:e"
+      it "should encode a dictionary with an empty list value" $
+       encodeBEncode (BDict [("foo", BList [])]) `shouldBe` "d3:foolee"
+      it "should encode a dictionary with an empty dictionary value" $
+        encodeBEncode (BDict [("foo", BDict [])]) `shouldBe` "d3:foodee"
+      it "should encode a dictionary with multiple key-value pairs, sorted" $
+        encodeBEncode (BDict [("z", BInt 3), ("a", BInt 1), ("m", BInt 2)]) `shouldBe` "d1:ai1e1:mi2e1:zi3ee"
