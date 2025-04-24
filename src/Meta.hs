ee8a6e9a9267010d7394978e782f14daf990551b
rodfer0x80 <rodrigolf080@gmail.com>
2025-04-24 17:40:36 +0100

    hashes

--- src/Meta.hs
+++ src/Meta.hs
@@ -5,9 +5,10 @@ module Meta (parseMeta) where
 import BEncode (BEncode (..), encodeBEncode)
 import qualified Crypto.Hash.SHA1 as SHA1
 import Data.ByteString (ByteString)
-import Data.ByteString.Base16 (encode)
+import qualified Data.ByteString.Base16 as Hex
 import qualified Data.ByteString.Char8 as B
 import Data.Maybe (fromMaybe)
+import Data.Bits (testBit)
 
 extractInteger :: BEncode -> Maybe Integer
 extractInteger (BInt i) = Just i
@@ -25,8 +26,16 @@ lookupBEncode :: ByteString -> [(ByteString, BEncode)] -> Maybe BEncode
 lookupBEncode key dict = lookup key dict
 
 hashBDict :: BEncode -> String
-hashBDict (BDict dict) = B.unpack $ encode $ SHA1.hash $ encodeBEncode (BDict dict)
+hashBDict (BDict dict) = B.unpack $ Hex.encode $ SHA1.hash $ encodeBEncode (BDict dict)
 hashBDict _ = ""
+
+processPieceHashes :: ByteString -> String
+processPieceHashes hashesBytes
+  | B.null hashesBytes = ""
+  | otherwise =
+    let (currentHash, remainingHashes) = B.splitAt 20 hashesBytes
+        hexHash = Hex.encode currentHash
+    in B.unpack hexHash ++ "\n" ++ processPieceHashes remainingHashes
 
 parseMeta :: BEncode -> String
 parseMeta (BDict metadata) =
@@ -50,5 +59,27 @@ parseMeta (BDict metadata) =
       infoHashStr = case infoDictMaybeBEncode of
         Just infoDictBEncode -> "Info Hash: " ++ hashBDict infoDictBEncode
         Nothing -> "Info Hash: N/A"
-   in trackerStr ++ "\n" ++ lengthStr ++ "\n" ++ infoHashStr ++ "\n"
+
+      pieceLengthStr = case infoDictMaybe of
+        Just infoDict -> case lookupBEncode "piece length" infoDict >>= extractInteger of
+          Just piecesLengthInt -> "Pieces Length: " ++ show piecesLengthInt
+          Nothing -> "Pieces Length: N/A"
+        Nothing -> "Pieces Length: N/A"
+
+      pieceHashesStr = case infoDictMaybe of
+        Just infoDict -> case lookupBEncode "pieces" infoDict >>= extractString of
+          Just hashesBytes -> "Piece Hashes:\n" ++ processPieceHashes hashesBytes -- unlines (map (B.unpack . encode) (chunksOf 20 hashesBytes))
+          Nothing -> "Pieces: N/A"
+        Nothing -> "Pieces: N/A"
+   in "\n"
+        ++ trackerStr
+        ++ "\n"
+        ++ lengthStr
+        ++ "\n"
+        ++ infoHashStr
+        ++ "\n"
+        ++ pieceLengthStr
+        ++ "\n"
+        ++ pieceHashesStr
+        ++ "\n"
 parseMeta _ = "Error: Metadata is not a BEncoded dictionary\n"
