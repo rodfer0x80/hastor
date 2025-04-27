86153f6c38c991d95409dd976ebeed6a432cfff2
rodfer0x80 <rodrigolf080@gmail.com>
2025-04-27 15:55:58 +0100

    get request working done

--- src/Meta.hs
+++ src/Meta.hs
@@ -8,11 +8,11 @@ import Data.Bits (testBit)
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Base16 as Hex
 import qualified Data.ByteString.Char8 as B
-import qualified Data.ByteString.Lazy.UTF8 as BSL
 import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString.Lazy.UTF8 as BSL
 import Data.Maybe (fromMaybe)
-import Network.URI.Encode as URL
 import Network.HTTP.Simple
+import Network.URI.Encode as URL
 
 extractInteger :: BEncode -> Maybe Integer
 extractInteger (BInt i) = Just i
@@ -61,16 +61,20 @@ parseInfoDict (BDict metadata) =
       infoDictMaybe = infoDictMaybeBEncode >>= extractDict
    in infoDictMaybe
 
-parseInfoHash :: BEncode -> String
-parseInfoHash (BDict metadata) = B.unpack $ Hex.encode $ hashBDict (BDict metadata)
+parseInfoHash :: BEncode -> Maybe ByteString
+parseInfoHash (BDict metadata) = do
+  infoDictBEncode <- lookupBEncode "info" metadata
+  return $ hashBDict infoDictBEncode
 
-parseinfoHashUrlEncoded :: BEncode -> String
-parseinfoHashUrlEncoded (BDict metadata) = B.unpack $ URL.encodeByteString $ hashBDict (BDict metadata)
+parseinfoHashUrlEncoded :: BEncode -> Maybe String
+parseinfoHashUrlEncoded (BDict metadata) = do
+  infoHash <- parseInfoHash metadata
+  return $ B.unpack $ URL.encodeByteString infoHash
 
 getPeers :: BEncode -> String
 getPeers (BDict metadata) =
   let trackerUrl = fromMaybe "" (parseTrackerUrl (BDict metadata))
-      urlEncodedHash = parseinfoHashUrlEncoded (BDict metadata)
+      urlEncodedHashMaybe = parseinfoHashUrlEncoded metadata
       peerId = "13374204204204201337"
       port = "6881"
       uploaded = "0"
@@ -81,35 +85,38 @@ getPeers (BDict metadata) =
           Nothing -> "0"
         Nothing -> "0"
       compact = "1"
-   in "tracker_url: "
-        ++ trackerUrl
-        ++ "\n"
-        ++ "info_hash: "
-        ++ urlEncodedHash
-        ++ "\n"
-        ++ "peer_id: "
-        ++ peerId
-        ++ "\n"
-        ++ "port: "
-        ++ port
-        ++ "\n"
-        ++ "uploaded: "
-        ++ uploaded
-        ++ "\n"
-        ++ "downloaded: "
-        ++ downloaded
-        ++ "\n"
-        ++ "left: "
-        ++ left
-        ++ "\n"
-        ++ "compact: "
-        ++ compact
-        ++ "\n"
+   in case urlEncodedHashMaybe of
+        Just urlEncodedHash ->
+          "tracker_url: "
+            ++ trackerUrl
+            ++ "\n"
+            ++ "info_hash: "
+            ++ urlEncodedHash
+            ++ "\n"
+            ++ "peer_id: "
+            ++ peerId
+            ++ "\n"
+            ++ "port: "
+            ++ port
+            ++ "\n"
+            ++ "uploaded: "
+            ++ uploaded
+            ++ "\n"
+            ++ "downloaded: "
+            ++ downloaded
+            ++ "\n"
+            ++ "left: "
+            ++ left
+            ++ "\n"
+            ++ "compact: "
+            ++ compact
+            ++ "\n"
+        Nothing -> "Error: Could not extract or encode info_hash\n"
 
 requestPeers :: BEncode -> IO String
 requestPeers (BDict metadata) = do
   let trackerUrl = fromMaybe "" (parseTrackerUrl (BDict metadata))
-      urlEncodedHash = parseinfoHashUrlEncoded (BDict metadata)
+      urlEncodedHashMaybe = parseinfoHashUrlEncoded metadata
       peerId = "13374204204204201337"
       port = "6881"
       uploaded = "0"
@@ -120,20 +127,33 @@ requestPeers (BDict metadata) = do
           Nothing -> "0"
         Nothing -> "0"
       compact = "1"
-      queryString = "?info_hash=" ++ urlEncodedHash ++
-                    "&peer_id=" ++ peerId ++
-                    "&port=" ++ port ++
-                    "&uploaded=" ++ uploaded ++
-                    "&downloaded=" ++ downloaded ++
-                    "&left=" ++ left ++
-                    "&compact=" ++ compact
-      fullUrl = trackerUrl ++ queryString
-  let request = parseRequest_ fullUrl
-  response <- httpLBS request
-  let statusCode = getResponseStatusCode response
-      lazyBody = getResponseBody response
-      decodedBody = decodeBEncode $ L.toStrict lazyBody
-  return ("Status Code: " ++ show statusCode ++ "\nBody (Decoded):\n" ++ show decodedBody ++ "\n")
+  case urlEncodedHashMaybe of
+    Just urlEncodedHash -> do
+      let queryString =
+            "?info_hash="
+              ++ urlEncodedHash
+              ++ "&peer_id="
+              ++ peerId
+              ++ "&port="
+              ++ port
+              ++ "&uploaded="
+              ++ uploaded
+              ++ "&downloaded="
+              ++ downloaded
+              ++ "&left="
+              ++ left
+              ++ "&compact="
+              ++ compact
+          fullUrl = trackerUrl ++ queryString
+      let request = parseRequest_ fullUrl
+      response <- httpLBS request
+      let statusCode = getResponseStatusCode response
+        lazyBody = getResponseBody response
+        decodedBody = decodeBEncode $ L.toStrict lazyBody
+      case decodedBodyResult of
+        Just decoded -> return ("Status Code: " ++ show statusCode ++ "\nBody (Decoded):\n" ++ show decoded ++ "\n")
+        Nothing -> return ("Status Code: " ++ show statusCode ++ "\nError decoding body.\n")
+    Nothing -> return "Error: Could not generate URL for tracker request (invalid info_hash).\n"
 
 parseMeta :: BEncode -> String
 parseMeta (BDict metadata) =
@@ -145,8 +165,8 @@ parseMeta (BDict metadata) =
           Nothing -> "Length: N/A"
         Nothing -> "Length: N/A"
 
-      infoHashStr = case (lookupBEncode "info" metadata) of
-        Just infoDictBEncode -> "Info Hash: " ++ (B.unpack (hashBDict infoDictBEncode))
+      infoHashStr = case parseInfoHash metadata of
+        Just infoDictBEncode -> "Info Hash: " ++ (B.unpack (Hex.encode hashBytes)) 
         Nothing -> "Info Hash: N/A"
 
       pieceLengthStr = case parseInfoDict (BDict metadata) of
